9a1b456bd4a6f39beaf5c7b63b334f3593745f4c
Josue Castillo <isaac9900.jc@gmail.com>
2023-11-07 10:10:51 -0400

    Portafolio-Navbar

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+import { Cover } from "@/components/Cover"
+import { Transition } from "@/components/Transition/Transition"
+
+export const metadata:Metadata = {
+  title:'Portafolio JosueDev Next 13',
+  description:'Portafolio Animate'
+}
+
+export default function Home() {
+  return (
+    <main className="bg-[#393A47] h-[100vh]">
+        <Transition/>
+        <Cover/>
+    </main>
+  )
+}
